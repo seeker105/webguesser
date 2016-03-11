d090f26e0c227c0c206d0b3443c61f69e8cd1d28
Christopher Soden <karavol27@gmail.com>
2016-03-11 11:38:41 -0700

    I4: Colors are working

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,45 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
-secret_number = rand(100)
+
+# secret_number = rand(100)
+secret_number = 20
 guess = nil
 
 
 get '/' do
   guess = params["guess"]
   local_message = check_guess(guess, secret_number)
-  erb :index, locals: {number: secret_number, message: local_message}
-end
-
-def check_guess(guess_string, secret_number)
-  guess = guess_string.to_i
-  if guess == 0
-    "Take a guess!"
-  elsif guess > secret_number
-    (guess - secret_number) > 5 ? "Way Too High" : "Too High"
-  elsif guess < secret_number
-    (secret_number - guess) > 5 ? "Way Too Low" : "Too Low"
-    # binding.pry
-  else
-    "You win! The secret number is #{secret_number}"
+  erb :index, locals: {number: secret_number, message: local_message, background: @background}
   end
-end
+
+  def check_guess(guess_string, secret_number)
+    guess = guess_string.to_i
+    if guess == 0
+      "Take a guess!"
+    elsif guess > secret_number
+      if (guess - secret_number) > 5
+        @background = "red"
+        return "Way Too High"
+        # bright red
+      else
+        @background = "rgb(233, 164, 164)"
+        return "Too High"
+        # light red
+      end
+    elsif guess < secret_number
+      if (secret_number - guess) > 5
+        @background = "red"
+        return "Way Too Low"
+        # bright red
+      else
+        @background = "rgb(233, 164, 164)"
+        return "Too Low"
+        # light red
+        # binding.pry
+      end
+    else
+      @background = "rgb(96, 242, 106)"
+      return "You win! The secret number is #{secret_number}"
+    end
+  end
